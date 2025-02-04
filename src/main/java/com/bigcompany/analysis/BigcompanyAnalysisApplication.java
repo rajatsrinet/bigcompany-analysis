ae379c56d969a33927a02015281d99a78a6f93db
rajat <rajatsrinet@gmail.com>
2025-02-04 12:01:58 +0530

    initial commit

---
+++ src/main/java/com/bigcompany/analysis/BigcompanyAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.bigcompany.analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigcompanyAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigcompanyAnalysisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bigcompany/analysis/BigcompanyAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigcompany.analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigcompanyAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
